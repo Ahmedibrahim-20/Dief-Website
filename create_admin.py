1d4fd45cf2f29fc80fc9d590759df83d36b36440
Ahmed <ahmedelwahab86@gmail.com>
2026-07-28 00:16:25 +0300

    Create create_admin.py

---
+++ create_admin.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import django
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dief.settings')
+
+try:
+    django.setup()
+    from django.contrib.auth.models import User
+
+    NEW_USERNAME = "admin2026"
+    NEW_PASSWORD = "MyStrongPassword2026!"
+    NEW_EMAIL = "admin@example.com"
+
+    user, created = User.objects.get_or_create(username=NEW_USERNAME, defaults={'email': NEW_EMAIL})
+    user.set_password(NEW_PASSWORD)
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+
+    if created:
+        print("--> Superuser created successfully!")
+    else:
+        print("--> Password for existing user was updated successfully!")
+
+except Exception as e:
+    print(f"--> Error running create_admin script: {e}")
