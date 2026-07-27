ea0a3eee95db43faf527c923be33578eb1b221b0
Ahmed <ahmedelwahab86@gmail.com>
2026-07-28 00:07:14 +0300

    Refactor create_admin.py for better error handling

--- Dief/create_admin.py
+++ Dief/create_admin.py
@@ -1,22 +1,30 @@
 import os
+import sys
 import django
 
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Dief.settings')
-django.setup()
 
-from django.contrib.auth.models import User
+try:
+    django.setup()
+    from django.contrib.auth.models import User
 
-NEW_USERNAME = "admin2026"
-NEW_PASSWORD = "MyStrongPassword2026!" 
-NEW_EMAIL = "admin@example.com"
+    NEW_USERNAME = "admin2026"
+    NEW_PASSWORD = "MyStrongPassword2026!"
+    NEW_EMAIL = "admin@example.com"
 
-user, created = User.objects.get_or_create(username=NEW_USERNAME, defaults={'email': NEW_EMAIL})
-user.set_password(NEW_PASSWORD)
-user.is_superuser = True
-user.is_staff = True
-user.save()
+    user, created = User.objects.get_or_create(username=NEW_USERNAME, defaults={'email': NEW_EMAIL})
+    user.set_password(NEW_PASSWORD)
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
 
-if created:
-    print("New Superuser created successfully!")
-else:
-    print("Password for existing user was updated successfully!")
+    if created:
+        print("--> Superuser created successfully!")
+    else:
+        print("--> Password for existing user was updated successfully!")
+
+except Exception as e:
+    print(f"--> Error running create_admin script: {e}")
